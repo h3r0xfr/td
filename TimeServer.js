ac28f7acc82667e3eb91b15d0b8b24a4290772e2
Camille LEGEY <31740065+Toundras@users.noreply.github.com>
2019-05-21 11:29:38 +0200

    Create TimeServer.js

---
+++ TimeServer.js
@@ -0,0 +1,9 @@
+const net = require('net');
+const moment = require('moment');
+const port = process.argv[2];
+
+const server = net.createServer((socket) => {
+    socket.end(moment().format('YYYY-MM-DD HH:mm') + '\n');
+});
+
+server.listen(port);
